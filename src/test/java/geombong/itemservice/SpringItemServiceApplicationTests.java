cfe31eb4fa22ef40cf95a68bf7482bdfd6842334
geombong <shoy1415@gmail.com>
2022-03-10 15:11:09 +0900

    git init

---
+++ src/test/java/geombong/itemservice/SpringItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package geombong.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
